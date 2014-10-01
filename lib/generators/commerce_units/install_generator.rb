d61b74cfa12bbcf02c7de3bc9f597e333aa6905d
Thomas Chen <foxnewsnetwork@gmail.com>
2014-09-30 23:35:31 -0700

    put in next_migration_number

--- lib/generators/commerce_units/install_generator.rb
+++ lib/generators/commerce_units/install_generator.rb
@@ -7,6 +7,17 @@ module CommerceUnits
 
       source_root File.expand_path('../templates/migrations', __FILE__)
 
+      # Define the next_migration_number method (necessary for the migration_template method to work)
+      # Stolen shamelessly from socery gem's generators
+      def self.next_migration_number(dirname)
+        if ActiveRecord::Base.timestamped_migrations
+          sleep 1 # make sure each time we get a different timestamp
+          Time.new.utc.strftime("%Y%m%d%H%M%S")
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
+
       def generate_migration
         migration_template "create_commerce_units_dimensions.rb.erb", "db/migrate/#{migration_file_name}"
       end
